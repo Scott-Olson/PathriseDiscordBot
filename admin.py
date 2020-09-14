9ac3a9f52b0ed949f2c0110017bf5d910dc635f8
Archaengel <god11341258@gmail.com>
2020-09-14 14:54:25 -0700

    improvement: Change spaces in admin.py to tabs

--- admin.py
+++ admin.py
@@ -4,23 +4,23 @@ from csv import DictWriter
 import os
 
 def generate_csv(members: List[Member]) -> str:
-  if not os.path.isdir("output"):
-    os.mkdir("output")
+	if not os.path.isdir("output"):
+		os.mkdir("output")
 
-  humans = filter(lambda m: not m.bot, members)
-  readable_humans = map(lambda m: {
-      "ID": m.id,
-      "Name": m.name,
-      "Display Name": m.display_name,
-      "Joined At": m.joined_at,
-    }, humans)
+	humans = filter(lambda m: not m.bot, members)
+	readable_humans = map(lambda m: {
+		"ID": m.id,
+		"Name": m.name,
+		"Display Name": m.display_name,
+		"Joined At": m.joined_at,
+	}, humans)
 
-  filename = "output/compatriats.csv"
-  fields = ["ID", "Name", "Display Name", "Joined At"]
-  file = open(filename, "w")
-  writer = DictWriter(file, fieldnames=fields)
-  writer.writeheader()
-  writer.writerows(readable_humans)
-  file.close()
+	filename = "output/compatriats.csv"
+	fields = ["ID", "Name", "Display Name", "Joined At"]
+	file = open(filename, "w")
+	writer = DictWriter(file, fieldnames=fields)
+	writer.writeheader()
+	writer.writerows(readable_humans)
+	file.close()
 
-  return filename
+	return filename
