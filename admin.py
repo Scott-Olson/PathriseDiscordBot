02a8da5e4727d5d314c50f5e873ade6bef7dc466
Archaengel <god11341258@gmail.com>
2020-09-14 13:35:15 -0700

    Generate basic csv file from guild members

--- admin.py
+++ admin.py
@@ -1,13 +1,15 @@
 from discord import Member
 from typing import List
 from csv import DictWriter
-from os import mkdir
+import os
 
 def generate_csv(members: List[Member]):
-  mkdir("output")
+  if not os.path.isdir("output"):
+    os.mkdir("output")
+
   filename = "output/test.csv"
-  file = open(filename, "w")
   fields = ["Display Name", "Nickname"]
+  file = open(filename, "w")
   writer = DictWriter(file, fieldnames=fields)
   writer.writeheader()
   writer.writerows(list(map(lambda m: dict({"Display Name": m.display_name, "Nickname": m.nick if m.nick is not None else "None" }), filter(lambda m: not m.bot, members))))
